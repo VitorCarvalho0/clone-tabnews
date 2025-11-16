e08609d808df6d2a89c07bd237d1112697258cbf
Vitor Carvalho <euvitorcarvalho0@gmail.com>
2025-11-16 11:08:17 -0300

    add 'database' and '.env'

---
+++ infra/database.js
@@ -0,0 +1,19 @@
+import { Cliente } from "pg";
+
+async function query(queryObject) {
+  const client = new Cliente({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
